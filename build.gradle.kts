bb397108a30cfb2f24acca114bccd45751b3fd21
Jezreel Joshua <jezreeljoshuam@gmail.com>
2022-06-16 23:31:36 +0800

    add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val exposed_version: String by project
+val postgres_version: String by project
+val hikari_version: String by project
+val bcrypt_version: String by project
 
 plugins {
     application
@@ -34,4 +38,14 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+
+    implementation("org.postgresql:postgresql:$postgres_version")
+
+    implementation ("com.zaxxer:HikariCP:$hikari_version")
+
+    implementation ("at.favre.lib:bcrypt:$bcrypt_version")
 }
