166256d9be9842e281724ed359f22d81a94855bc
Jezreel Joshua <jezreeljoshuam@gmail.com>
2022-06-18 17:55:25 +0800

    add class for retrieving environment variables

---
+++ src/main/kotlin/io/github/jezreal/configuration/StoreAccountConfiguration.kt
@@ -0,0 +1,8 @@
+package io.github.jezreal.configuration
+
+data class StoreAccountConfiguration(
+    val username: String,
+    val password: String,
+    val storeName: String,
+    val storeAddress: String
+)
