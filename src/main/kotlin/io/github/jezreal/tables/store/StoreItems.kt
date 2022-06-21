31da14a74e1a8545e4f4caf4d1c1aad0213da459
Jezreel Joshua <jezreeljoshuam@gmail.com>
2022-06-21 21:11:55 +0800

    add tables

--- src/main/kotlin/io/github/jezreal/tables/store/StoreItems.kt
+++ src/main/kotlin/io/github/jezreal/tables/store/StoreItems.kt
@@ -1,15 +1,17 @@
 package io.github.jezreal.tables.store
 
+import io.github.jezreal.tables.item.ItemCategories
+import io.github.jezreal.tables.item.Items
 import io.github.jezreal.tables.price.Prices
 import org.jetbrains.exposed.sql.Table
 
 object StoreItems : Table() {
     val storeItemId = long("store_item_id").autoIncrement()
-    val storeItemName = varchar("store_item_name", 500)
     val storeItemAmount = long("store_item_amount")
+    val itemId = long("item_id").references(Items.itemId)
     val priceId = long("price_id").references(Prices.priceId)
     val storeId = long("store_id").references(Stores.storeId)
-    val storeItemCategoryId = long("store_item_category_id").references(StoreItemCategories.storeItemCategoryId)
+    val itemCategoryId = long("store_item_category_id").references(ItemCategories.itemCategoryId)
 
     override val primaryKey: PrimaryKey
         get() = PrimaryKey(storeItemId)
