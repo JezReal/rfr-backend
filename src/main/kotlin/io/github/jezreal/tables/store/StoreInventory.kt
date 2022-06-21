ea99046fcbc75dc7b4a857f9ffdcd241e66dc1e2
Jezreel Joshua <jezreeljoshuam@gmail.com>
2022-06-21 22:42:40 +0800

    rename table field names

--- src/main/kotlin/io/github/jezreal/tables/store/StoreInventory.kt
+++ src/main/kotlin/io/github/jezreal/tables/store/StoreInventory.kt
@@ -5,11 +5,11 @@ import io.github.jezreal.tables.item.Items
 import org.jetbrains.exposed.sql.Table
 
 object StoreInventory : Table() {
-    val storeItemId = long("store_item_id").autoIncrement()
-    val storeItemAmount = long("store_item_amount")
+    val storeItemId = long("store_inventory_id").autoIncrement()
+    val storeItemAmount = long("item_amount")
     val itemId = long("item_id").references(Items.itemId)
     val storeId = long("store_id").references(Stores.storeId)
-    val itemCategoryId = long("store_item_category_id").references(ItemCategories.itemCategoryId)
+    val itemCategoryId = long("item_category_id").references(ItemCategories.itemCategoryId)
 
     override val primaryKey: PrimaryKey
         get() = PrimaryKey(storeItemId)
