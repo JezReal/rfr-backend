296dfa4a1fd3904699978823ba1c1128c6a44737
Jezreel Joshua <jezreeljoshuam@gmail.com>
2022-06-22 21:48:20 +0800

    add category id in dto

--- src/main/kotlin/io/github/jezreal/item/dto/ItemPriceByCategoryDto.kt
+++ src/main/kotlin/io/github/jezreal/item/dto/ItemPriceByCategoryDto.kt
@@ -1,6 +1,10 @@
 package io.github.jezreal.item.dto
 
+import com.google.gson.annotations.SerializedName
+
 data class ItemPriceByCategoryDto(
+    @SerializedName("category_id")
+    val categoryId: Long,
     val category: String,
     val items: List<ItemPriceWithIdDto>
 )
