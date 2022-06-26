3b6c82ae4ffeaf782ba389f49f85265fa56141f7
Jezreel Joshua <jezreeljoshuam@gmail.com>
2022-06-26 21:43:54 +0800

    sign and encrypt refresh token

--- src/main/kotlin/io/github/jezreal/plugins/Sessions.kt
+++ src/main/kotlin/io/github/jezreal/plugins/Sessions.kt
@@ -3,15 +3,20 @@ package io.github.jezreal.plugins
 import io.github.jezreal.auth.dto.RefreshToken
 import io.ktor.server.application.*
 import io.ktor.server.sessions.*
+import io.ktor.util.*
 
 fun Application.configureSession() {
     val day = (60 * 60 * 24).toLong() // 24 hours in seconds
     val week = day * 7
 
+    val secretEncryptKey = hex(environment.config.property("ktor.security.secureKeys.encryptKey").getString())
+    val secretSignKey = hex(environment.config.property("ktor.security.secureKeys.signKey").getString())
+
     install(Sessions) {
         cookie<RefreshToken>("refresh-token") {
             cookie.httpOnly = true
             cookie.maxAgeInSeconds = week
+            transform(SessionTransportTransformerEncrypt(secretEncryptKey, secretSignKey))
         }
     }
 }
